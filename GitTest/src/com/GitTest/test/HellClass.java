b6d1c4ea172e455c8ec4618fa35271f99f5e5273
Aymane ELBELKACEMI <aymen.elbelkacemi@gmail.com>
2014-11-09 20:59:57 +0000

    Initial commit Hello MotherFuckers

---
+++ GitTest/src/com/GitTest/test/HellClass.java
@@ -0,0 +1,11 @@
+package com.GitTest.test;
+
+public class HellClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello MotherFucker");
+
+	}
+
+}
