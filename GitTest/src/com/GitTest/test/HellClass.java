b334b79beff23b2e3e1f15337d49b620aee2d21c
Aymane ELBELKACEMI <aymen.elbelkacemi@gmail.com>
2014-11-09 23:24:56 +0000

    Crating new classe and printing the value of a merge

--- GitTest/src/com/GitTest/test/HellClass.java
+++ GitTest/src/com/GitTest/test/HellClass.java
@@ -4,7 +4,9 @@ public class HellClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		NumberChange v1= new NumberChange(); 
 		System.out.println("Hello MotherFucker");
+		System.out.println("this is :"+v1.a);
 		System.out.println("goodbye MotherFucker");
 
 	}
---
+++ GitTest/src/com/GitTest/test/NumberChange.java
@@ -0,0 +1,9 @@
+package com.GitTest.test;
+
+public class NumberChange {
+	public int a;
+	
+public NumberChange(){
+	this.a=0;
+}
+}
