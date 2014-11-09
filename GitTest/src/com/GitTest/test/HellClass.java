e8d2a8149249fa6decbae7fc0bac2a44444500a4
Aymane ELBELKACEMI <aymen.elbelkacemi@gmail.com>
2014-11-09 21:14:48 +0000

    first change

--- GitTest/src/com/GitTest/test/HellClass.java
+++ GitTest/src/com/GitTest/test/HellClass.java
@@ -5,6 +5,7 @@ public class HellClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello MotherFucker");
+		System.out.println("goodbye MotherFucker");
 
 	}
 
